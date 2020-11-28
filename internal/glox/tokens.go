8edc6a7e375d950b885627f3499fb01cc3d0466c
Corentin Dupret <32418149+nockty@users.noreply.github.com>
2020-11-28 17:57:46 +0100

    🏷️ Add Token type
    Eventually, we'll need to figure out what needs to be
    private and what needs to be exported.

---
+++ internal/glox/tokens.go
@@ -0,0 +1,68 @@
+package glox
+
+// TokenType TODO
+type TokenType int
+
+const (
+	// Single-character tokens
+	LeftParen TokenType = iota
+	RightParen
+	LeftBrace
+	RightBrace
+	Comma
+	Dot
+	Minus
+	Plus
+	Semicolon
+	Slash
+	Star
+	// One or two character tokens
+	Bang
+	BangEqual
+	Equal
+	EqualEqual
+	Greater
+	GreaterEqual
+	Less
+	LessEqual
+	// Literals
+	Identifier
+	String
+	Number
+	// Keywords
+	And
+	Class
+	Else
+	False
+	Fun
+	For
+	If
+	Nil
+	Or
+	Print
+	Return
+	Super
+	This
+	True
+	Var
+	While
+	// EOF
+	EOF
+)
+
+// Token TODO
+type Token struct {
+	TokenType TokenType
+	Lexeme    string
+	Literal   interface{}
+	Line      int
+}
+
+func NewToken(tokenType TokenType, lexeme string, literal interface{}, line int) *Token {
+	return &Token{
+		TokenType: tokenType,
+		Lexeme:    lexeme,
+		Literal:   literal,
+		Line:      line,
+	}
+}
