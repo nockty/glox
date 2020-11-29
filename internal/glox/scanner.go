c76fc40216e42b55d0aa162d2ab0896999084842
Corentin Dupret <32418149+nockty@users.noreply.github.com>
2020-11-30 00:47:58 +0100

    ⚗ Add most of the scanner's logic

---
+++ internal/glox/scanner.go
@@ -0,0 +1,225 @@
+package glox
+
+import "strconv"
+
+var keywords = map[string]TokenType{
+	"and":    And,
+	"class":  Class,
+	"else":   Else,
+	"false":  False,
+	"fun":    Fun,
+	"for":    For,
+	"if":     If,
+	"nil":    Nil,
+	"or":     Or,
+	"print":  Print,
+	"return": Return,
+	"super":  Super,
+	"this":   This,
+	"true":   True,
+	"var":    Var,
+	"while":  While,
+}
+
+type Scanner struct {
+	source string
+	tokens []*Token
+	// first character in the lexeme being scanned
+	start int
+	// character currently being considered
+	current int
+	// source line where the current character is
+	line int
+}
+
+func NewScanner(source string) *Scanner {
+	return &Scanner{
+		source: source,
+		line:   1,
+	}
+}
+
+func (s *Scanner) scanTokens() {
+	for !s.isAtEnd() {
+		// Beginning of the next lexeme
+		s.start = s.current
+		s.scanToken()
+	}
+	s.tokens = append(s.tokens, NewToken(EOF, "", nil, s.line))
+}
+
+func (s *Scanner) scanToken() {
+	c := s.advance()
+	switch c {
+	case '(':
+		s.addToken(LeftParen, nil)
+	case ')':
+		s.addToken(RightParen, nil)
+	case '{':
+		s.addToken(LeftBrace, nil)
+	case '}':
+		s.addToken(RightBrace, nil)
+	case ',':
+		s.addToken(Comma, nil)
+	case '.':
+		s.addToken(Dot, nil)
+	case '-':
+		s.addToken(Minus, nil)
+	case '+':
+		s.addToken(Plus, nil)
+	case ';':
+		s.addToken(Semicolon, nil)
+	case '*':
+		s.addToken(Star, nil)
+	case '!':
+		if s.match('=') {
+			s.addToken(BangEqual, nil)
+		} else {
+			s.addToken(Bang, nil)
+		}
+	case '=':
+		if s.match('=') {
+			s.addToken(EqualEqual, nil)
+		} else {
+			s.addToken(Equal, nil)
+		}
+	case '<':
+		if s.match('=') {
+			s.addToken(LessEqual, nil)
+		} else {
+			s.addToken(Less, nil)
+		}
+	case '>':
+		if s.match('=') {
+			s.addToken(GreaterEqual, nil)
+		} else {
+			s.addToken(Greater, nil)
+		}
+	case '/':
+		if s.match('/') {
+			// a comment goes until the end of the line
+			for s.peek() != '\n' && !s.isAtEnd() {
+				s.advance()
+			}
+		} else {
+			s.addToken(Slash, nil)
+		}
+	case ' ', '\r', '\t':
+		// ignore whitespace
+		break
+	case '\n':
+		s.line++
+	case '"':
+		s.string()
+	default:
+		if isDigit(c) {
+			s.number()
+		} else if isAlpha(c) {
+			s.identifier()
+		} else {
+			// TODO error here
+		}
+	}
+}
+
+func (s *Scanner) advance() byte {
+	s.current++
+	return s.source[s.current-1]
+}
+
+func (s *Scanner) match(expected byte) bool {
+	if s.isAtEnd() {
+		return false
+	}
+	if s.source[s.current] != expected {
+		return false
+	}
+	s.current++
+	return true
+}
+
+func (s *Scanner) peek() byte {
+	if s.isAtEnd() {
+		return '\x00'
+	}
+	return s.source[s.current]
+}
+
+func (s *Scanner) peekNext() byte {
+	if s.current+1 >= len(s.source) {
+		return '\x00'
+	}
+	return s.source[s.current+1]
+}
+
+func (s *Scanner) string() {
+	for s.peek() != '"' && !s.isAtEnd() {
+		if s.peek() == '\n' {
+			s.line++
+		}
+		s.advance()
+	}
+	if s.isAtEnd() {
+		// TODO error here
+		return
+	}
+	// closing "
+	s.advance()
+	// trim surrounding quotes
+	value := s.source[s.start+1 : s.current-2]
+	s.addToken(String, value)
+}
+
+func (s *Scanner) number() {
+	for isDigit(s.peek()) {
+		s.advance()
+	}
+	// look for a fractional part
+	if s.peek() == '.' && isDigit(s.peekNext()) {
+		// consume the '.'
+		s.advance()
+		for isDigit(s.peek()) {
+			s.advance()
+		}
+	}
+	value, err := strconv.ParseFloat(s.source[s.start:s.current-1], 64)
+	if err != nil {
+		// TODO what to do here? Out of range float
+	}
+	s.addToken(Number, value)
+}
+
+func (s *Scanner) identifier() {
+	for isAlphaNumeric(s.peek()) {
+		s.advance()
+	}
+	// check for reserved keywords
+	text := s.source[s.start : s.current-1]
+	if tokenType, ok := keywords[text]; ok {
+		s.addToken(tokenType, nil)
+	}
+	s.addToken(Identifier, nil)
+}
+
+func (s *Scanner) addToken(tokenType TokenType, literal interface{}) {
+	text := s.source[s.start : s.current-1]
+	s.tokens = append(s.tokens, NewToken(tokenType, text, literal, s.line))
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= len(s.source)
+}
+
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+func isAlpha(c byte) bool {
+	return (c >= 'a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z') ||
+		c == '_'
+}
+
+func isAlphaNumeric(c byte) bool {
+	return isDigit(c) || isAlpha(c)
+}
